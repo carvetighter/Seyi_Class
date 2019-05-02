5b1446f026c2e8ff779afdf16b05ec04454337d9
Brent Allen <carvetighter@gmail.com>
2019-05-01 23:53:35 -0500

    added test for nuron file

---
+++ test_nuron.py
@@ -0,0 +1,41 @@
+'''
+file documentation
+'''
+###############################################
+###############################################
+#
+# File / Package Import
+#
+###############################################
+###############################################
+
+from time import time
+from matplotlib import pyplot
+import pandas
+import os
+
+import warnings
+warnings.simplefilter('ignore')
+
+###############################################
+###############################################
+#
+# path variables
+#
+###############################################
+###############################################
+
+string_path = 'C:\\Code\\\Development\\Python\\Seyi_Class\\data'
+string_file_test_raw = 'test_technidus_clf.csv'
+string_file_train_raw = 'train_technidus_clf.csv'
+
+###############################################
+###############################################
+#
+# load data
+#
+###############################################
+###############################################
+
+df_test_raw = pandas.read_csv(os.path.join(string_path, string_file_test_raw))
+df_train_raw = pandas.read_csv(os.path.join(string_path, string_file_train_raw))
